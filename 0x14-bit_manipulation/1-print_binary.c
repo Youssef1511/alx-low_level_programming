2699152790b8ac5dc013803d42900854d984e819
Youssef <myyoussef2003@hotmail.com>
2023-09-28 16:11:53 -0700

    e

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,4 +6,23 @@
  */
 void print_binary(unsigned long int n)
 {
+	int z, cnt = 0;
+	unsigned long int curr;
+
+	for (z = 63; z >= 0; z--)
+	{
+		curr = n >> z;
+
+		if (curr & 1)
+		{
+			_putchar('1');
+			cnt++;
+		}
+		else if (cnt)
+			_putchar('0');
+	}
+	if (!cnt)
+		_putchar('0');
+}
+
 
