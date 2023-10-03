c0f453d52656bdf621ea9f0362840841735e4e00
Youssef <myyoussef2003@hotmail.com>
2023-10-02 18:49:30 -0700

    e

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,19 @@
  *        0 when function fails or filename is NULL.
  */
 
-ssize_t read_textfile (const char *filename, size_t letters)
-{	
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 	int fd;
 	ssize_t bytes;
-	char buf [READ_BUF_SIZE * 8];
-	
+	char buf[READ_BUF_SIZE * 8];
+
 	if (!filename || !letters)
-		return (O);
-	fd = open (filename, O_RDONLY);
+		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd. &buf [0]. letters):
-	bytes = write (STDOUT_FILENO, &buf [0], bytes);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (bytes);
 }
